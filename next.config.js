20f0b06f49b77f80e99c637142822075f0acea40
Jean Guillemont <127295741+JeanGuillemont@users.noreply.github.com>
2023-12-12 16:34:20 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   images: {
     remotePatterns: [
       {
@@ -13,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = { nextConfig, output: "export" };
+module.exports = nextConfig;
