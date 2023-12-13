60d48b128043f343f09df856216a1862dec3ba38
Jean Guillemont <127295741+JeanGuillemont@users.noreply.github.com>
2023-12-13 21:24:13 +0000

    Update fetchZora.ts

--- pages/api/fetchZora.ts
+++ pages/api/fetchZora.ts
@@ -14,73 +14,70 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  const fetchZora = async () => {
-    try {
-      // call tokenChat to fetch last NFT minted on Zora and parse html response with cheerio
-      const response = await fetch("https://tokenchat.co/new-today");
-      const html = await response.text();
-      const $ = cheerio.load(html);
-      const h2Elements = $("h2 a").toArray();
-      let selectedString;
-      let attempts = 0;
+  try {
+    // Fetch and parse HTML from the main page
+    const response = await fetch("https://tokenchat.co/new-today");
+    const html = await response.text();
+    const $ = cheerio.load(html);
 
-      while (!selectedString && attempts < h2Elements.length) {
-        //select a random elements parsing made before (a random new NFT minted on Zora)
-        const randomIndex = Math.floor(Math.random() * h2Elements.length);
-        const href = $(h2Elements[randomIndex]).attr("href");
-        let stringAfterSecondSlash;
-        if (href !== undefined) {
-          const splitHref = href.split("/");
-          stringAfterSecondSlash = splitHref.slice(2).join("/");
-        }
-        // validate NFT selected only if on Optimism, Zora or base network
-        if (
+    // Filter valid elements
+    const validElements = $("h2 a")
+      .toArray()
+      .filter((element) => {
+        const href = $(element).attr("href");
+        const stringAfterSecondSlash = href?.split("/").slice(2).join("/");
+        return (
           stringAfterSecondSlash &&
           (stringAfterSecondSlash.startsWith("zora") ||
             stringAfterSecondSlash.startsWith("base") ||
             stringAfterSecondSlash.startsWith("oeth"))
-        ) {
-          selectedString = stringAfterSecondSlash;
+        );
+      });
 
-          // Fetch the page for the selected string
-          const pageResponse = await fetch(
-            `https://tokenchat.co/collection/${selectedString}`,
-          );
-          const pageHtml = await pageResponse.text();
-          const page$ = cheerio.load(pageHtml);
+    // Select a random valid element
+    const randomIndex = Math.floor(Math.random() * validElements.length);
+    const selectedString = $(validElements[randomIndex])
+      .attr("href")
+      ?.split("/")
+      .slice(2)
+      .join("/");
 
-          // Extract the image, title, and creator, fileType and size
-          const image = page$(".thumb img").attr("src");
-          const title = page$("h1 span:nth-child(1) a").text();
-          const creator = page$(".collection-desc .commenter a").text();
-          const fileType = page$(".file-type").text();
-          const size = page$(".media-info span:nth-child(2)").text();
+    // Fetch and parse HTML from the selected page
+    const pageResponse = await fetch(
+      `https://tokenchat.co/collection/${selectedString}`,
+    );
+    const pageHtml = await pageResponse.text();
+    const page$ = cheerio.load(pageHtml);
 
-          if (
-            image !== undefined &&
-            title !== undefined &&
-            creator !== undefined &&
-            fileType !== undefined &&
-            size !== undefined
-          ) {
-            res.status(200).json({
-              token: selectedString,
-              image,
-              title,
-              creator,
-              fileType,
-              size,
-            });
-          }
-        }
+    // Extract the image, title, and creator, fileType and size
+    const image = page$(".thumb img").attr("src");
+    const title = page$("h1 span:nth-child(1) a").text();
+    const creator = page$(".collection-desc .commenter a").text();
+    const fileType = page$(".file-type").text();
+    const size = page$(".media-info span:nth-child(2)").text();
 
-        attempts++;
-      }
-    } catch (error) {
-      console.error(error); // Log the error for debugging
-      res.status(500);
+    if (
+      image !== undefined &&
+      title !== undefined &&
+      creator !== undefined &&
+      fileType !== undefined &&
+      size !== undefined
+    ) {
+      res.status(200).json({
+        token: selectedString,
+        image,
+        title,
+        creator,
+        fileType,
+        size,
+      });
+    } else {
+      throw new Error("Failed to extract data");
     }
-  };
-
-  await fetchZora();
+  } catch (error) {
+    console.error(error); // Log the error for debugging
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing your request" });
+  }
 }
