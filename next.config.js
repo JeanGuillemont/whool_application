a271ac0eb46d0c73907325821d10619dcdb3ba7a
Jean Guillemont <127295741+JeanGuillemont@users.noreply.github.com>
2023-12-13 09:07:43 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  output: "export",
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
