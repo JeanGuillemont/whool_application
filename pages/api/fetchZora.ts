ffd9f988d3b73bdced1b483579b9271bbdcc6b1e
Jean Guillemont <127295741+JeanGuillemont@users.noreply.github.com>
2023-12-19 14:19:48 +0000

    Update fetchZora.ts

--- pages/api/fetchZora.ts
+++ pages/api/fetchZora.ts
@@ -62,7 +62,9 @@ const fetchZora = async () => {
     title !== undefined &&
     creator !== undefined &&
     fileType !== undefined &&
-    size !== undefined
+    size !== undefined &&
+    fileType !== "TEXT" &&
+    fileType !== "3D SCENE"
   ) {
     return {
       token: selectedString as any,
